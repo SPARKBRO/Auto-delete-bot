0a4a2d169b6876baace80407705d57aedab1c6fc
Midhun <103011794+SPARKBRO@users.noreply.github.com>
2025-01-08 00:22:57 +0530

    Create info.py

---
+++ utils/info.py
@@ -0,0 +1,12 @@
+import os
+
+API_ID       = int(os.environ.get("API_ID", ""))
+API_HASH     = os.environ.get("API_HASH", "")
+BOT_TOKEN    = os.environ.get("BOT_TOKEN", "")
+SESSION      = os.environ.get("SESSION", "")
+TIME         = int(os.environ.get("TIME", 10))
+CHATS        = [int(cht) for cht in os.environ.get("CHATS", "").split()]
+WHITE_LIST   = [int(wht) for wht in os.environ.get("WHITE_LIST", "").split()]
+BLACK_LIST   = [int(blk) for blk in os.environ.get("BLACK_LIST", "").split()]
+DATABASE_URI = os.environ.get("DATABASE_URI", "")
+PORT         = os.environ.get("PORT", "8080")
